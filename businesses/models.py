4eccaa7ea8a593f29a2d2abb4660cb336af9fea6
Adam <adamthomasjackson3@gmail.com>
2020-08-17 17:14:02 -0400

    Created the models

--- businesses/models.py
+++ businesses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Business(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    email = models.EmailField
+    logo = models.ImageField
+
+
+
+class FeatureProduct(models.Model):
+    belongsTo = models.ForeignKey(Business, blank=True, null=True, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=200)
+    shipping_time = models.CharField(max_length=200)
+    price = models.DecimalField
+    product_image = models.ImageField
+    product_link = models.CharField(max_length=200)
